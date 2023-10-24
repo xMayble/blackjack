83fb98163a73ba528953149236a37e1869f43f21
Mayble <mehburohit@gmail.com>
2023-10-24 19:53:56 -0400

    added documentation

--- blackjack.py
+++ blackjack.py
@@ -9,13 +9,19 @@ intro = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ")
 # if the user wants to play, we show interface and give them a set of 2 cards!
 if intro == 'y':
     # -insert blackjack logo here-
+
+    # randomize first two cards
     card1, card2 = random.choice(cards), random.choice(cards)
+    # if the first two cards sum to over 21, we randomize again
     while card1 + card2 > 21:
         card1 = random.choice(cards)
         card2 = random.choice(cards)
+    # create an empty list for the cards
     user_cards = []
+    # append the randomized cards to list
     user_cards.append(card1)
     user_cards.append(card2)
+    # show user their new set of cards
     print(f"Your cards are: {user_cards}")
 
 
