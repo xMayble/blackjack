b173ca4b6379b141dd5312a3cb354173fadbc553
Mayble <mehburohit@gmail.com>
2023-10-24 19:51:40 -0400

    give user their first set of cards

--- blackjack.py
+++ blackjack.py
@@ -1,2 +1,22 @@
 import random
 
+# card values we are dealing with
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
+# ask user if they want to play
+intro = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ")
+
+# if the user wants to play, we show interface and give them a set of 2 cards!
+if intro == 'y':
+    # -insert blackjack logo here-
+    card1, card2 = random.choice(cards), random.choice(cards)
+    while card1 + card2 > 21:
+        card1 = random.choice(cards)
+        card2 = random.choice(cards)
+    user_cards = []
+    user_cards.append(card1)
+    user_cards.append(card2)
+    print(f"Your cards are: {user_cards}")
+
+
+
